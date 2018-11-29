2ce9edd20b5077c4745967913c0cb73f69c093df
RED <redetroyder@gmail.com>
2018-11-29 18:43:00 +0300

    Initial Fixes

--- app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
@@ -15,77 +15,63 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Вызывается при нажатии пользователем на кнопку Решить */
-        public void solveEquation(View view) {
-             // ax+b=c
-            double a = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_a)).getText().toString());
-            double b = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_b)).getText().toString());
-            double c = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_c)).getText().toString());
-            TextView result = (TextView) findViewById(R.id.result);
-            result.setText("" + String.valueOf((c - b) / a));
+    /**
+     * Вызывается при нажатии пользователем на кнопку Решить
+     */
+    public void solveEquation(View view) {
+        // ax+b=c
+        double a = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_a)).getText().toString());
+        double b = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_b)).getText().toString());
+        double c = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_c)).getText().toString());
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText("" + String.valueOf((c - b) / a));
+    }
+
+    // Solving double square equasion through D
+    public void solveDoubleEquasion(View view) {
+        double a = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_a2)).getText().toString());
+        double b = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_b2)).getText().toString());
+        double c = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_c2)).getText().toString());
+
+        /*
+         * D = b^2 - 4ac
+         *
+         * D>0 - 2
+         * D=0 - 1
+         * D<0 - 0
+         */
+
+        double D = b * b - 4 * a * c;
+        double x = 0.0;
+        double x2 = 0.0;
+
+        boolean plusMinus = false;
+        boolean exception = false;
+
+        String resultString = "";
+        if (a == 0) {
+            resultString = "x = " + String.valueOf(-c / b);
+        } else if (D > 0) {
+            x = (Math.sqrt(D) - b) / (2 * a);
+            x2 = (-Math.sqrt(D) - b) / (2 * a);
+            resultString = "x1 = " + x + "; x2 = " + x2;
+        } else if (D == 0) {
+            x = (Math.sqrt(D) - b) / (2 * a);
+            resultString = "x = " + x;
+        } else {
+            resultString = "D less than zero";
         }
 
-        // Solving double square equasion through D
-        public void solveDoubleEquasion(View view) {
-            // get cf's
-            // KFC :ROFLAN_ZDAROVA:
-            double a = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_a2)).getText().toString());
-            double b = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_b2)).getText().toString());
-            double c = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_c2)).getText().toString());
 
-            /*
-             * D = b^2 - 4ac
-             *
-             * D>0 - 2
-             * D=0 - 1
-             * D<0 - 0
-            */
+        TextView result = (TextView) findViewById(R.id.result2);
+        result.setText(resultString);
 
-            double D = b*b - 4*a*c; // General formula for D
-            double x = 0.0;
-            double x2 = 0.0;
-
-            boolean plusMinus = false; // For x index 1 and x index 2
-            boolean exception = false; // If D less than zero
-
-            String resultString = "";
-
-            // Checking for simple equasion
-            if (a!=0) {
-                // Checking D values
-                if (D>0) {
-                    x = (Math.sqrt(D)-b)/(2*a);
-                    x2 = (-Math.sqrt(D)-b)/(2*a);
-                    plusMinus = true;
-                } else if (D==0) {
-                    x = (Math.sqrt(D)-b)/(2*a);
-                } else {
-                    exception = true;
-                }
-
-                // Boolean checks
-                if (plusMinus) {
-                    resultString = "x1 = " + x + "; x2 = " + x2;
-                } else if (exception) {
-                    resultString = "D less than zero";
-                } else {
-                    resultString = "x = " + x;
-                }
-            } else {
-                // Here we are solving a simple equasion
-                resultString = "x = " + String.valueOf(-c/b);
-            }
-
-
-            TextView result = (TextView) findViewById(R.id.result2);
-            result.setText(resultString);
-
-        }
+    }
 
 }
